bbadcc1320f99b4abff497c28002a2dadb0a3f1c
yulia <polushka.yulia@gmail.com>
2018-12-11 20:31:14 +0300

    Start Project

---
+++ Operators_10_12_2018/Operators_10_12_2018/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_10_12_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
